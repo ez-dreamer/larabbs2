7c27fd6ebc2b5f759e03e4441adee591eb4fb4d0
ez-dreamer <ez_dreamer@163.com>
2020-08-12 12:28:01 +0000

    话题列表页面

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
+@section('title', '话题列表')
+
 @section('content')
-<div class="container">
+  <div class="row mb-5">
+    <div class="col-md-9 col-lg-9 topic-list">
+      <div class="card ">
+        <div class="card-header bg-transparent">
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link active" href="#">最后回复</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">最新发布</a></li>
+          </ul>
+        </div>
+
+        <div class="card-body">
+          {{-- 话题列表 --}}
+          @include('topics._topic_list', ['topics' => $topics])
+          {{-- 分页 --}}
+          <div class="mt-5">
+            {!! $topics->appends(Request::except('page'))->render() !!}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-lg-3 sidebar">
+      @include('topics._sidebar')
+    </div>
+  </div>
+
+{{--<div class="container">
   <div class="col-md-10 offset-md-1">
     <div class="card ">
       <div class="card-header">
@@ -56,6 +84,6 @@
       </div>
     </div>
   </div>
-</div>
+</div>--}}
 
 @endsection
