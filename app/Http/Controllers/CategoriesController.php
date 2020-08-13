bb0ffe9df998ae7fa6e4186f54ab2cf3aca80a9c
ez-dreamer <ez_dreamer@163.com>
2020-08-13 10:25:32 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Request $request, Category $category)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::query()->where('category_id', $category->id)->paginate(20);
+        $topics = Topic::withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->with('category', 'user')
+            ->paginate(20);
 
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
