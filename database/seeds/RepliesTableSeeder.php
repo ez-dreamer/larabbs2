7bc45b25e7bdaefc18b3dc870528e1a895250226
ez-dreamer <ez_dreamer@163.com>
2020-08-17 10:49:25 +0000

    回复列表

--- database/seeds/RepliesTableSeeder.php
+++ database/seeds/RepliesTableSeeder.php
@@ -19,7 +19,7 @@ class RepliesTableSeeder extends Seeder
         // 获取 Faker 实例
         $faker = app(\Faker\Generator::class);
 
-        $replies = factory(Reply::class)->times(50)->make()->each(function ($reply, $index) use ($faker, $user_ids, $topic_ids) {
+        $replies = factory(Reply::class)->times(1000)->make()->each(function ($reply, $index) use ($faker, $user_ids, $topic_ids) {
             // 从用户 ID 数组中随机取出一个并赋值
             $reply->user_id = $faker->randomElement($user_ids);
 
