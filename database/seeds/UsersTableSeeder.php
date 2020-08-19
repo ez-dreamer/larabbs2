73d20f6ddd56c426422caae4da34b412d60bdddb
ez-dreamer <ez_dreamer@163.com>
2020-08-19 05:23:44 +0000

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,9 +17,20 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Eleven';
-        $user->email = 'eleven@example.com';
+        $user->name = 'EZ-dreamer';
+        $user->email = 'ez_dreamer@163.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->name = 'Eleven';
+        $user->email = 'eleven@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png';
+        $user->save();
+        $user->assignRole('Maintainer');
     }
 }
