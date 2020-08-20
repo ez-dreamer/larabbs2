67557dc342d959bcf57d4d0fa7f0cf6b64732e09
ez-dreamer <ez_dreamer@163.com>
2020-08-20 11:44:32 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -69,6 +69,11 @@ return array(
             'users',
             'roles',
             'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
         ]
     ],
 
